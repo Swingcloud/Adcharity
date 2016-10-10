74b4509900fe2c2dd4bedc005603c363c0d53336
Sam Lin <greenlin111@gmail.com>
2016-10-10 15:02:51 +0800

    add Facebook Omniauth login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -35,7 +35,9 @@ Devise.setup do |config|
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [:email]
-
+  fb_config =Rails.application.config_for(:facebook)
+  config.omniauth :facebook, fb_config["app_id"], fb_config["secret"], :scope => 'public_profile,email', :info_fields => 'email,name', callback_url: fb_config['callback_url']
+  
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
   # find_for_authentication method and considered in your model lookup. For instance,
