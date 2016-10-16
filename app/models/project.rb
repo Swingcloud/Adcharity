9931da58ef4c4e7646d3cb0f73067450dae51622
Sam Lin <greenlin111@gmail.com>
2016-10-16 20:29:02 +0800

    revise css and add partail

--- app/models/project.rb
+++ app/models/project.rb
@@ -15,5 +15,6 @@ class Project < ApplicationRecord
   default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  scope :newest_article, ->{ order("created_at DESC")}
 
 end
