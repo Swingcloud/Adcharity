56b00d02a7f5def051b1c6ca1ad037209a81181c
Sam Lin <greenlin111@gmail.com>
2016-10-17 12:24:22 +0800

    add method to Omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,13 +7,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      if @user.email.nil?
+        # 如果 User 沒有提供 email 刪除 User 的 app 讓 User 重新授權
+
+        @user.delete_access_token(request.env["omniauth.auth"])
+        redirect_to new_user_registration_url, alert: "需要您同意 Email 授權唷！"
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
   end
 
   def failure
-    redirect_to root_path
+    redirect_to root_path, alert: "無法獲得驗證！"
   end
 
   
